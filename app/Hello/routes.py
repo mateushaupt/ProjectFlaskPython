88a971ac70f8bc59a22f7ec55c29ddce589b9bac
Mateus Haupt <mateus.haupt@gmail.com>
2025-09-08 20:25:05 -0300

    feat: criação usuario

--- app/Hello/routes.py
+++ app/Hello/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect
 from ..models import User, db
 hello_bp = Blueprint('hello', __name__)
 
@@ -7,6 +7,18 @@ def index():
     users = User.query.all()
     return render_template('index.html', users=users)
 
+@hello_bp.route('/newUser', methods=['POST'])
+def newUser():
+    username = request.form['username']
+
+    newUser = User(username=username)
+    db.session.add(newUser)
+    db.session.commit()
+
+    return redirect('/')
+
+
+
 @hello_bp.route('/sobre')
 def about():
-    return "Olá, Mateus"
+    return "Olá, Gremista nato!"
